40a7a2739c04bdaf12ae3019cca7dc4807fdb47e
Thiago Suchorski <suchorski@ymail.com>
2024-04-23 09:34:40 -0300

    Fix wrong link description for next page on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <h1>Example of Index page</h1>
-    <NuxtLink to="/next">Go to index page</NuxtLink>
+    <NuxtLink to="/next">Go to next page</NuxtLink>
   </section>
 </template>
 
